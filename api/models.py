bea357bd538529d09e6e22ae4460a677c8338e11
Sixuan Wu <wusixuan2@icloud.com>
2024-08-02 20:18:16 -0400

    add created_at to table persons, add seed data

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.name
